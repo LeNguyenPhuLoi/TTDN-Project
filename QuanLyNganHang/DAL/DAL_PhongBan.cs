6337107f715edf65c9e191b3231c767208807dd7
KhanhDang <nkdan2005@gmail.com>
2025-06-13 10:02:48 +0700

    Thêm autoconnect

--- QuanLyNganHang/DAL/DAL_PhongBan.cs
+++ QuanLyNganHang/DAL/DAL_PhongBan.cs
@@ -10,7 +10,13 @@ namespace DAL
     public class DAL_PhongBan
     {
         //Kết nối với Linq to SQL
-        QLNHDataContext db = new QLNHDataContext();
+        AutoConnect conn = new AutoConnect();
+        QLNHDataContext db;
+
+        public DAL_PhongBan()
+        {
+            db = new QLNHDataContext();
+        }
 
         //Lấy ds phòng ban
         public IQueryable LoadDSPB()
