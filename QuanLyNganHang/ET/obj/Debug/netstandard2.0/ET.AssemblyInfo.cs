4d2d4f919c610d16a4e226a626e1ead191f8fa96
LeNguyenVi1457 <23211tt3822@mail.tdc.edu.vn>
2025-07-19 10:44:01 +0700

    Update Form Tài Khoản

--- QuanLyNganHang/ET/obj/Debug/netstandard2.0/ET.AssemblyInfo.cs
+++ QuanLyNganHang/ET/obj/Debug/netstandard2.0/ET.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86b7f23e977d702543a1ccc60224d352821200bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d5e863397536b97d36afefd414de039564babca")]
 [assembly: System.Reflection.AssemblyProductAttribute("ET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
